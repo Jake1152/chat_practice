8f2920725468252c6f285bb6717ec7d44f2623a4
Jake1152 <rancho974@gmail.com>
2023-06-01 18:53:44 +0900

    CH
    add WebSocket
    
    WebSocket on the http server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 const cors = require('cors');
 const app = express();
@@ -9,6 +11,19 @@ app.set('view engine', "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+// const handleListen = () => console.log('Listening on ws://localhost:3000');
+// app.listen(3000, handleListen);
+
+
+// http 서버 위에 webSocket서버를 만들 수 있게 만드는 코드
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });// not neccessary
+// 이렇게하면 http, websocket server 둘다 돌릴 수 있다
+// 함께 http, ws 를 만들지 않아도 된다.
+server.listen(3000, handleListen);
+// 서버는 http, ws 두가지 프로토콜을 이해할 수 있게 되었다.
